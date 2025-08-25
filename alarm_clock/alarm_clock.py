88b78485bfcf34a3db227c9463b753c8c49c1bd6
studpallpa2739 <pallvi.pallvi@stud.th-rosenheim.de>
2025-08-25 20:23:46 +0530

    Build Alarm clock

---
+++ alarm_clock/alarm_clock.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+import time
+
+#ANSI
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed+=1
+
+        time_left = seconds-time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
+    playsound(r"C:\Users\Admin\Desktop\Python_Projects\alarm_clock\sound.mp3")
+
+minutes = int(input("HOw many minutes to wait: "))
+seconds= int(input("How many seconds to wait: "))
+total_seconds = (minutes * 60) + seconds
+
+alarm(total_seconds)
